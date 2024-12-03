2c21c399a659bb996a9f7fae4b9574bc38b95354
S0G3K1NG <daviddoichita@proton.me>
2024-12-03 09:30:05 +0100

    script base de datos

--- bbdd.sql
+++ bbdd.sql
@@ -1,8 +1,10 @@
-drop database if exists guardias;
-create database guardias;
+DROP DATABASE IF EXISTS guardias;
+CREATE DATABASE guardias;
 
-DROP TABLE IF EXISTS PROFESOR;
-CREATE TABLE PROFESOR (
+USE guardias;
+
+DROP TABLE IF EXISTS profesor;
+CREATE TABLE profesor (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(32),
     apellidos VARCHAR(32),
@@ -14,39 +16,77 @@ CREATE TABLE PROFESOR (
     email VARCHAR(256)
 );
 
-DROP TABLE IF EXISTS MATERIA;
-CREATE TABLE MATERIA (
+DROP TABLE IF EXISTS materia;
+CREATE TABLE materia (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(32)
 );
 
-DROP TABLE IF EXISTS MATERIAPROFESOR;
-CREATE TABLE MATERIAPROFESOR (
+DROP TABLE IF EXISTS materiaprofesor;
+CREATE TABLE materiaprofesor (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     idprofesor INT,
     idmateria INT,
-    FOREIGN KEY(idprofesor) REFERENCES PROFESOR(id),
-    FOREIGN KEY(idmateria) REFERENCES MATERIA(id)
+    FOREIGN KEY(idprofesor) REFERENCES profesor(id),
+    FOREIGN KEY(idmateria) REFERENCES materia(id)
 );
 
-DROP TABLE IF EXISTS SESION;
-CREATE TABLE SESION (
+DROP TABLE IF EXISTS grupo;
+CREATE TABLE grupo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(10),
+    idtutor INT,
+    FOREIGN KEY(idtutor) REFERENCES profesor(id)
+);
+
+DROP TABLE IF EXISTS intervalo;
+CREATE TABLE intervalo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    horainicio TIME,
+    horafin TIME
+);
+
+DROP TABLE IF EXISTS sesion;
+CREATE TABLE sesion (
     id INT AUTO_INCREMENT PRIMARY KEY,
     idprofesor INT,
     idgrupo INT,
     idintervalo INT,
     idmateria INT,
     aula VARCHAR(16),
-    FOREIGN KEY(idprofesor) REFERENCES PROFESOR(id),
-    FOREIGN KEY(idgrupo) REFERENCES GRUPO(id),
-    FOREIGN KEY(idintervalo) REFERENCES INTERVALO(id),
-    FOREIGN KEY(idmateria) REFERENCES MATERIA(id)
+    FOREIGN KEY(idprofesor) REFERENCES profesor(id),
+    FOREIGN KEY(idgrupo) REFERENCES grupo(id),
+    FOREIGN KEY(idintervalo) REFERENCES intervalo(id),
+    FOREIGN KEY(idmateria) REFERENCES materia(id)
 );
 
-DROP TABLE IF EXISTS GRUPO;
-CREATE TABLE GRUPO (
+DROP TABLE IF EXISTS cargo;
+CREATE TABLE cargo (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(10),
-    idtutor INT,
-    FOREIGN KEY(tutor) REFERENCES PROFESOR(id)
+    nombre VARCHAR(10)
+);
+
+DROP TABLE IF EXISTS curso;
+CREATE TABLE curso (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(10)
+);
+
+DROP TABLE IF EXISTS cuadrante;
+CREATE TABLE cuadrante (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idintervalo INT,
+    idcargo INT,
+    idcurso INT,
+    idprofesor INT,
+    idsesion INT,
+    incidencias TEXT,
+    firma TEXT,
+    deberes TEXT,
+    fecha DATE,
+    FOREIGN KEY(idintervalo) REFERENCES intervalo(id),
+    FOREIGN KEY(idcargo) REFERENCES cargo(id),
+    FOREIGN KEY(idcurso) REFERENCES curso(id),
+    FOREIGN KEY(idprofesor) REFERENCES profesor(id),
+    FOREIGN KEY(idsesion) REFERENCES sesion(id)
 );
