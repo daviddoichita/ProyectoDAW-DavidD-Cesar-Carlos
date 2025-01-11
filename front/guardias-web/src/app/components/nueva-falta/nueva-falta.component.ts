4e1d0f9341408da91af21ea91f45383d2906658f
carlosripolles <aw.carlosripolles@gmail.com>
2025-01-11 18:13:07 +0100

    Nueva Falta front

--- front/guardias-web/src/app/components/nueva-falta/nueva-falta.component.ts
+++ front/guardias-web/src/app/components/nueva-falta/nueva-falta.component.ts
@@ -3,18 +3,57 @@ import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { FloatLabelModule } from 'primeng/floatlabel'
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-nueva-falta',
   standalone: true,
-  imports: [DropdownModule, CalendarModule, CommonModule, FormsModule, FloatLabelModule, TableModule, ButtonModule],
+  imports: [DropdownModule, CalendarModule, CommonModule, 
+    FormsModule, FloatLabelModule, TableModule, ButtonModule],
   templateUrl: './nueva-falta.component.html',
-  styleUrl: './nueva-falta.component.scss'
+  styleUrls: ['./nueva-falta.component.scss']
 })
 export class NuevaFaltaComponent {
   rangeDates: Date[] | undefined;
 
+  horariosManana = [
+    { indice: 1, hora: '8:20 - 9:15' },
+    { indice: 2, hora: '9:15 - 10:10' },
+    { indice: 3, hora: '10:10 - 11:00' },
+    { hora: '11:00 - 11:25' },
+    { indice: 4, hora: '11:25 - 12:20' },
+    { indice: 5, hora: '12:20 - 13:10' },
+    { indice: 6, hora: '13:10 - 14:00' },
+    { hora: '14:00 - 14:10' },
+    { indice: 7, hora: '14:10 - 15:00' },
+  ];
+
+  horariosTarde = [
+    { indice: 1, hora: '15:15 - 16:05' },
+    { indice: 2, hora: '16:05 - 17:00' },
+    { indice: 3, hora: '17:00 - 17:50' },
+    { hora: '17:50 - 18:10' },
+    { indice: 4, hora: '18:10 - 19:00' },
+    { indice: 5, hora: '19:00 - 19:50' },
+    { indice: 6, hora: '19:50 - 20:40' },
+    { hora: '20:40 - 20:50' },
+    { indice: 7, hora: '20:50 - 21:40' },
+  ];
+
+  asignaturasSeleccionadas = [
+    {
+      deberes: false,
+      nombre: 'Programacion',
+      hora: '8:20 - 9:15',
+      dia: 'Lunes',
+    },
+    {
+      deberes: true,
+      nombre: 'bbdd',
+      hora: '9:15 - 10:10',
+      dia: 'Martes',
+    },
+  ];
 }
