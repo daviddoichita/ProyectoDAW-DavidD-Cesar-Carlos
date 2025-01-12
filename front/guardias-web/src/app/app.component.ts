46170e44a1aab91f89f35f835942f4c655c0342e
daviddoichita <davdoi@alu.edu.gva.es>
2025-01-12 16:42:40 +0100

    Estado utilizable header y sidebar

--- front/guardias-web/src/app/app.component.ts
+++ front/guardias-web/src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- front/guardias-web/src/app/components/cuadrante-profesor/cuadrante-profesor.component.ts
+++ front/guardias-web/src/app/components/cuadrante-profesor/cuadrante-profesor.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-cuadrante-profesor',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, PanelModule],
   templateUrl: './cuadrante-profesor.component.html',
-  styleUrl: './cuadrante-profesor.component.scss'
+  styleUrl: './cuadrante-profesor.component.scss',
 })
-export class CuadranteProfesorComponent {
-
-}
+export class CuadranteProfesorComponent {}
