f4190478013e9cdcd67d50f566e74b1e62d31261
daviddoichita <davdoi@alu.edu.gva.es>
2025-01-13 20:08:12 +0100

    Implemencation generacion cuadrantes y busqueda por rango

--- back/server-guardias/src/main/java/ies/camp/guardias/web/restController/CuadranteRestController.java
+++ back/server-guardias/src/main/java/ies/camp/guardias/web/restController/CuadranteRestController.java
@@ -1,13 +1,19 @@
 package ies.camp.guardias.web.restController;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ies.camp.guardias.model.dto.CuadranteDTO;
@@ -45,5 +51,34 @@ public class CuadranteRestController {
         log.info(this.getClass().getSimpleName() + " findById: devolver cuadrante con id: {}", id);
 
         return this.cuadranteService.findById(id);
+    }
+
+    /**
+     * Devuelve los CuadranteDTO en el rango introducido
+     * 
+     * @return lista de CuadranteDTO en el rango introducido
+     */
+    @PostMapping(path = "/range")
+    public List<CuadranteDTO> findByRange(@RequestParam LocalDate start, @RequestParam LocalDate end) {
+        log.info(this.getClass().getSimpleName() + " findByRange: devolver cuadrantes en el rango: [{}, {}]", start,
+                end);
+
+        return this.cuadranteService.findByRange(start, end);
+    }
+
+    /**
+     * Manda la peticion de generar cuadrantes al service
+     * 
+     * @param roles define si asignar roles al finalizar la generacion
+     * @return exito de la generacion
+     */
+    @GetMapping(path = "/generate/{roles}")
+    @ResponseBody
+    public Map<String, Boolean> generate(@PathVariable Boolean roles) {
+        log.info(this.getClass().getSimpleName() + " generate: generar cuadrantes");
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("exito", this.cuadranteService.generate(roles));
+        return response;
     }
 }
