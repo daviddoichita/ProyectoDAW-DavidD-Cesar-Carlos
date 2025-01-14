ae6a6b0c2102208cbb9698050264a8daf577cf1d
daviddoichita <davdoi@alu.edu.gva.es>
2025-01-14 18:11:18 +0100

    Pruebas

--- front/guardias-web/src/app/components/header/header.component.ts
+++ front/guardias-web/src/app/components/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
 import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +23,8 @@ import { Router } from '@angular/router';
     SplitButtonModule,
     InputTextModule,
     SidebarModule,
+    AvatarModule,
+    RippleModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -26,6 +36,6 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.titulo = this.router.url.split('/')[1].toUpperCase();
+    this.titulo = this.router.url.split('/')[1].toUpperCase().replace('-', ' ');
   }
 }
