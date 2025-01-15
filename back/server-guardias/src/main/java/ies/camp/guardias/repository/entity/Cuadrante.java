4c6faf4016d22194402e0bc585ec926d2042ffdd
daviddoichita <davdoi@alu.edu.gva.es>
2025-01-15 20:02:20 +0100

    hasCode() y equals() en Cuadrante

--- back/server-guardias/src/main/java/ies/camp/guardias/repository/entity/Cuadrante.java
+++ back/server-guardias/src/main/java/ies/camp/guardias/repository/entity/Cuadrante.java
@@ -49,4 +49,29 @@ public class Cuadrante {
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "sesionfalta", joinColumns = @JoinColumn(name = "idcuadrante"), inverseJoinColumns = @JoinColumn(name = "idsesion"))
     private Set<Sesion> faltas;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cuadrante other = (Cuadrante) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 }
