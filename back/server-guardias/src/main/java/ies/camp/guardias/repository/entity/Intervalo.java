64679aa0fe18689bf198c5313c5fa1aeadeec770
daviddoichita <davdoi@alu.edu.gva.es>
2025-01-15 18:32:25 +0100

    Mapeo Intervalo

---
+++ back/server-guardias/src/main/java/ies/camp/guardias/repository/entity/Intervalo.java
@@ -0,0 +1,65 @@
+package ies.camp.guardias.repository.entity;
+
+import java.time.LocalTime;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "intervalo")
+public class Intervalo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "horainicio")
+    private LocalTime horaInicio;
+    @Column(name = "horafin")
+    private LocalTime horaFin;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "intervalo")
+    @ToString.Exclude
+    private Set<Sesion> sesiones;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Intervalo other = (Intervalo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+}
