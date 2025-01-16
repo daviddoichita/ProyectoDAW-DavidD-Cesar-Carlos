4bcdd8b5df2869cd2dcedb983688cad9c0a8356e
carlosripolles <162994986+carlosripolles@users.noreply.github.com>
2025-01-16 12:54:42 +0100

    Terminado Back Listado Profesores (#12)

--- back/server-guardias/src/main/java/ies/camp/guardias/model/dto/SesionDTO.java
+++ back/server-guardias/src/main/java/ies/camp/guardias/model/dto/SesionDTO.java
@@ -2,9 +2,6 @@ package ies.camp.guardias.model.dto;
 
 import java.io.Serializable;
 
-import ies.camp.guardias.repository.entity.Aula;
-import ies.camp.guardias.repository.entity.Grupo;
-import ies.camp.guardias.repository.entity.Materia;
 import ies.camp.guardias.repository.entity.Sesion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,9 +30,6 @@ public class SesionDTO implements Serializable {
      * @return sesion convertida a DTO
      */
     public static SesionDTO convertToDTO(Sesion sesion) {
-        GrupoDTO grupoDTO = sesion.getGrupo() == null ? null : GrupoDTO.convertToDTO(sesion.getGrupo());
-        MateriaDTO materiaDTO = sesion.getMateria() == null ? null : MateriaDTO.convertToDTO(sesion.getMateria());
-        AulaDTO aulaDTO = sesion.getAula() == null ? null : AulaDTO.convertToDTO(sesion.getAula());
 
         return SesionDTO.builder()
                 .id(sesion.getId())
@@ -56,11 +50,6 @@ public class SesionDTO implements Serializable {
      * @return sesionDTO convertida a entidad
      */
     public static Sesion convertToEntity(SesionDTO sesionDTO) {
-        Grupo grupo = sesionDTO.getGrupoDTO() == null ? null : GrupoDTO.convertToEntity(sesionDTO.getGrupoDTO());
-        Materia materia = sesionDTO.getMateriaDTO() == null ? null
-                : MateriaDTO.convertToEntity(sesionDTO.getMateriaDTO());
-        Aula aula = sesionDTO.getAulaDTO() == null ? null : AulaDTO.convertToEntity(sesionDTO.getAulaDTO());
-
         return Sesion.builder()
                 .id(sesionDTO.getId())
                 .profesor(ProfesorDTO.convertToEntity(sesionDTO.getProfesor()))
---
+++ back/server-guardias/src/main/java/ies/camp/guardias/web/restController/ProfesorRestController.java
@@ -0,0 +1,74 @@
+package ies.camp.guardias.web.restController;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ies.camp.guardias.model.dto.ProfesorDTO;
+import ies.camp.guardias.service.ProfesorService;
+
+@RestController
+@RequestMapping("/api/profesores")
+public class ProfesorRestController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProfesorRestController.class);
+
+    @Autowired
+    private ProfesorService profesorService;
+
+    /**
+     * Devuelve todos los ProfesorDTO en formato JSON
+     *
+     * @return lista de ProfesorDTO
+     */
+    @GetMapping(path = "")
+    public List<ProfesorDTO> findAll() {
+        log.info(this.getClass().getSimpleName() + " findAll: devolver todos los profesores");
+
+        return this.profesorService.findAll();
+    }
+
+    /**
+     * Devuelve el ProfesorDTO con id introducido en formato JSON
+     *
+     * @param id ID del ProfesorDTO a buscar
+     * @return ProfesorDTO en formato JSON o null si no se encuentra la id
+     *         introducida
+     */
+    @GetMapping(path = "/{id}")
+    public ProfesorDTO findById(@PathVariable Long id) {
+        log.info(this.getClass().getSimpleName() + " findById: devolver profesor con id: {}", id);
+
+        return this.profesorService.findById(id);
+    }
+
+    /**
+     * Borra el Profesor con id introducido
+     *
+     * @param id ID del Profesor a borrar
+     */
+    @GetMapping(path = "/{id}/delete")
+    public void delete(@PathVariable Long id) {
+        log.info(this.getClass().getSimpleName() + " deleteById: borrar profesor con id: {}", id);
+
+        this.profesorService.delete(id);
+    }
+
+    /**
+     * Guarda el ProfesorDTO introducido en la base de datos
+     *
+     * @param profesorDTO ProfesorDTO a guardar
+     */
+    @GetMapping(path = "/{id}/save")
+    public void save(@PathVariable Long id) {
+        log.info(this.getClass().getSimpleName() + " save: guardar profesor con id: {}", id);
+
+        this.profesorService.save(this.profesorService.findById(id));
+    }
+}
