3eaaff918f3f99fe83064aeda89dbf9c18f5bfe9
S0G3K1NG <daviddoichita@proton.me>
2025-01-16 13:11:12 +0100

    Deficion tablas principales

--- front/guardias-web/src/app/components/cuadrante-profesor/cuadrante-profesor.component.ts
+++ front/guardias-web/src/app/components/cuadrante-profesor/cuadrante-profesor.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-cuadrante-profesor',
   standalone: true,
-  imports: [HeaderComponent, PanelModule],
+  imports: [HeaderComponent, PanelModule, TableModule],
   templateUrl: './cuadrante-profesor.component.html',
   styleUrl: './cuadrante-profesor.component.scss',
 })
-export class CuadranteProfesorComponent {}
+export class CuadranteProfesorComponent {
+  products = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+  ];
+}
