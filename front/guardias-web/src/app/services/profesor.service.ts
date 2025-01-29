097770f74e2b7a856efabc46101026337afd5d83
carlosripolles <aw.carlosripolles@gmail.com>
2025-01-29 13:21:17 +0100

    Arreglos

--- front/guardias-web/src/app/services/profesor.service.ts
+++ front/guardias-web/src/app/services/profesor.service.ts
@@ -1,36 +1,37 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesorService {
-  private apiUrl = 'http://localhost:8000/api/profesores';
+  private apiUrl = 'https://localhost:8000/api/profesores';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   findAll(): any {
     const headers = new HttpHeaders({
       'Authorization': 'Basic ' + btoa('admin:admin')
     });
 
-    return this.http.get<any[]>(this.apiUrl, { headers });
+    return this.http.get<any[]>(this.apiUrl, { headers: this.auth.getAuthHeader() });
   }
 
   findById(id: number): any {
     const headers = new HttpHeaders({
       'Authorization': 'Basic ' + btoa('admin:admin'),
     });
-  
-    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers });
+
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers: this.auth.getAuthHeader() });
   }
-  
+
   save(profesor: any): any {
     const headers = new HttpHeaders({
-        'Authorization': 'Basic ' + btoa('admin:admin')
+      'Authorization': 'Basic ' + btoa('admin:admin')
     });
 
-    return this.http.post(`${this.apiUrl}/save`, profesor, { headers });
+    return this.http.post(`${this.apiUrl}/save`, profesor, { headers: this.auth.getAuthHeader() });
   }
 
   delete(id: number): any {
@@ -38,7 +39,7 @@ export class ProfesorService {
       'Authorization': 'Basic ' + btoa('admin:admin')
     });
 
-    return this.http.delete(`${this.apiUrl}/${id}/delete`, { headers });
+    return this.http.delete(`${this.apiUrl}/${id}/delete`, { headers: this.auth.getAuthHeader() });
   }
 
   update(id: number, profesor: any): any {
@@ -46,6 +47,6 @@ export class ProfesorService {
       'Authorization': 'Basic ' + btoa('admin:admin')
     });
 
-    return this.http.put<any>(`${this.apiUrl}/${id}`, profesor, { headers });
+    return this.http.put<any>(`${this.apiUrl}/${id}`, profesor, { headers: this.auth.getAuthHeader() });
   }
 }
