e7eac281f9f39c66a72f5182a492e79acbd8e7d1
ceesartorres <151839354+ceesartorres@users.noreply.github.com>
2025-02-02 22:05:36 +0100

    Comienzo de gráficos para faltas

--- front/guardias-web/src/app/components/informes-faltas/informes-faltas.component.ts
+++ front/guardias-web/src/app/components/informes-faltas/informes-faltas.component.ts
@@ -10,17 +10,17 @@ import { InformesFaltasService } from '../../services/informes-faltas.service';
 import { SelectItem } from 'primeng/api';
 import { DropdownModule } from 'primeng/dropdown';
 import { SelectButtonModule } from "primeng/selectbutton";
-
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-informes-faltas',
   standalone: true,
-  imports: [HeaderComponent, TableModule, ButtonModule, InputTextModule, FormsModule, CommonModule, DropdownModule, SelectButtonModule],
+  imports: [HeaderComponent, TableModule, ButtonModule, InputTextModule, FormsModule, CommonModule, DropdownModule, SelectButtonModule, ChartModule],
   templateUrl: './informes-faltas.component.html',
   styleUrl: './informes-faltas.component.scss'
 })
 export class InformesFaltasComponent implements OnInit {
-  tipo: string = 'bar';
+  tipo: string = 'table';
   tipos: any[] = [];
 
   cuadrantes: Cuadrante[] = [];
@@ -28,33 +28,81 @@ export class InformesFaltasComponent implements OnInit {
   filtros: SelectItem[] = [];
   filtroSeleccionado: any;
 
+  barChartData: any;
+  lineChartData: any;
+  pieChartData: any;
 
   constructor(private informesFaltasService: InformesFaltasService) { }
 
   ngOnInit(): void {
     this.informesFaltasService.getCuadrantesConFaltas().subscribe(data => {
       this.cuadrantes = data;
+      this.initializeChartData();
     });
 
     this.filtros = [
-
       { label: 'Fecha', value: 'fecha' },
-
       { label: 'Mes', value: 'mes' }
-
     ];
 
     this.tipos = [
-      { name: 'pi pi-chart-bar', value: 'bar' },
-      { name: 'pi pi-chart-line', value: 'line' },
-      { name: 'pi pi-chart-pie', value: 'pie' }
+      { name: '', icon: 'pi pi-chart-bar', value: 'bar' },
+      { name: '', icon: 'pi pi-chart-line', value: 'line' },
+      { name: '', icon: 'pi pi-chart-pie', value: 'pie' },
+      { name: '', icon: 'pi pi-table', value: 'table' },
     ];
-
   }
 
   buscador() {
   }
 
   verFaltas(): void {
+  }
+
+  initializeChartData() {
+    this.barChartData = {
+      labels: ['Label1', 'Label2', 'Label3'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [65, 59, 80],
+          backgroundColor: '#42A5F5',
+        },
+        {
+          label: 'Dataset 2',
+          data: [28, 48, 40],
+          backgroundColor: '#66BB6A',
+        }
+      ]
+    };
+
+    this.lineChartData = {
+      labels: ['Label1', 'Label2', 'Label3'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [65, 59, 80],
+          fill: false,
+          borderColor: '#42A5F5'
+        },
+        {
+          label: 'Dataset 2',
+          data: [28, 48, 40],
+          fill: false,
+          borderColor: '#66BB6A'
+        }
+      ]
+    };
+
+    this.pieChartData = {
+      labels: ['Label1', 'Label2', 'Label3'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }
+      ]
+    };
   }
 }
