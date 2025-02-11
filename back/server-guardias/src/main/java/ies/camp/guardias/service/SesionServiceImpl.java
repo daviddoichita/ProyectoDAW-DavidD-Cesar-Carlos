e47ee4b19c298d60c39055ae8823f56c768984d9
daviddoichita <daviddoichita@proton.me>
2025-02-11 18:48:58 +0100

    Ejectuar carga csv de manera asincrona

--- back/server-guardias/src/main/java/ies/camp/guardias/service/SesionServiceImpl.java
+++ back/server-guardias/src/main/java/ies/camp/guardias/service/SesionServiceImpl.java
@@ -42,6 +42,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -91,6 +92,7 @@ public class SesionServiceImpl implements SesionService {
     private RolRepository rolRepository;
 
     @Override
+    @Async("async")
     public boolean loadFromCSV(MultipartFile csv, int year) {
         log.info(
                 this.getClass().getSimpleName() +
